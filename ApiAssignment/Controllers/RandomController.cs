99c1f4ae7bb6730b209b540a11dfb086f9871c7b
Rasheen <A00229247@mycambrian.ca>
2020-12-01 04:44:06 +0530

    Add project files.

---
+++ ApiAssignment/Controllers/RandomController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiAssignment.Helper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiAssignment.Controllers
+{
+
+
+    [Route("[controller]")]
+    [ApiController]
+    public class RandomController : ControllerBase
+    {
+        Utility utility = new Utility();
+
+        [HttpGet]
+        public dynamic get()
+        {
+            return utility.randomInt() ;
+        }
+
+
+        [HttpPost]
+        public dynamic post()
+        {
+            return  utility.randomDouble() ;
+        }
+
+
+        [HttpPut]
+        public dynamic put()
+        {
+            return "Thank you for the new number!";
+        }
+
+        [HttpDelete]
+        public dynamic delete()
+        {
+            return utility.randomCharacter();
+        }
+
+        [HttpPatch]
+        public dynamic patch()
+        {
+            return DateTime.Now;
+        }
+
+        [HttpOptions]
+        public dynamic options()
+        {
+            
+            return new  {GET = "Returns a random integer", 
+                POST = "Returns a random double",
+                PUT = "Returns a string saying 'Thank you for the new number!'",
+                DELETE = "Returns a random character",
+                PATCH = "Returns the current date" };
+        }
+    }
+}
---
+++ ApiAssignment/Helper/Utility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiAssignment.Helper
+{
+    public class Utility
+    {
+
+        public int randomInt()
+        {
+            Random rnd = new Random();
+            return rnd.Next(100);
+        }
+
+        public double randomDouble()
+        {
+            Random rnd = new Random();
+            double flt = rnd.NextDouble();
+            return Math.Truncate(flt * 100) / 100;
+        }
+
+        public char randomCharacter()
+        {
+            Random rnd = new Random();
+            double flt = rnd.NextDouble();
+            int shift = Convert.ToInt32(Math.Floor(25 * flt));
+            return  Convert.ToChar(shift + 65);
+        }
+
+    }
+}
